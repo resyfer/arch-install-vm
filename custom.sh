a31644dfa7e92cb91c0d5d4ab0695499e26b8dd8
resyfer <resyfer.dev@gmail.com>
2021-11-02 15:15:51 +0530

    fixed omf config path bug

--- custom.sh
+++ custom.sh
@@ -3,5 +3,5 @@
 ### Theme
 git clone https://github.com/resyfer/linux-config.git
 cp ./linux-config/fish/config.fish ~/.config/fish/
-\$OMF_CONFIG
+cd "$(dirname "$(\$OMF_CONFIG)")"
 cp ~/linux-config/fish/init.fish .
